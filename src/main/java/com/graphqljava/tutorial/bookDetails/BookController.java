fcf82db36a4ca20d32f619a060d83bf5c376d20b
José Flávio <joseflaviol119@gmail.com>
2022-08-27 17:41:00 -0300

    commit

---
+++ src/main/java/com/graphqljava/tutorial/bookDetails/BookController.java
@@ -0,0 +1,27 @@
+package com.graphqljava.tutorial.bookDetails;
+
+import org.springframework.graphql.data.method.annotation.Argument;
+import org.springframework.graphql.data.method.annotation.QueryMapping;
+import org.springframework.graphql.data.method.annotation.SchemaMapping;
+import org.springframework.stereotype.Controller;
+
+import java.util.List;
+
+@Controller
+public class BookController {
+
+    @QueryMapping(name = "allBooks")
+    public List<Book> allBooks() {
+        return Book.getBooks();
+    }
+
+    @QueryMapping(name = "bookById")
+    public Book bookById(@Argument String id) {
+        return Book.getById(id);
+    }
+
+    @SchemaMapping
+    public Author author(Book book) {
+        return Author.getById(book.getAuthorId());
+    }
+}
